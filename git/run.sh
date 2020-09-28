f3c658193837cf64f05ee3aa5b03d90d1fcbc6e5
wxnacy <wxnacy@gmail.com>
2020-09-29 01:06:59 +0800

    Update: 增加 git 过滤配置

--- git/run.sh
+++ git/run.sh
@@ -5,6 +5,7 @@
 git config --global user.name "${GIT_USERNAME}"
 git config --global user.email "${GIT_EMAIL}"
 git config --global credential.helper store
+echo "https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com" > ~/.git-credentials
 
 while true
 do
