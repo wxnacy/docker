53f42bfc65791ffc1d4c13279cb38c18169a734e
xiaoning.wen <xiaoning.wen@yiducloud.cn>
2020-10-05 15:13:08 +0800

    Add：Pyenv dockerfile

---
+++ pyenv/init.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Author: wxnacy(wxnacy@gmail.com)
+# Description:
+
+source ~/.bashrc
+cat ~/.bashrc
+/bin/bash -c " echo $PYENV_ROOT"
+/bin/bash -c " echo $PATH"
+/bin/bash pyenv install 2.7.13
