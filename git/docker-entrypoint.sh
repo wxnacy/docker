7222a7a45785a2c22b72fe8aa72c05d93abf2cd9
wxnacy <wxnacy@gmail.com>
2020-10-13 05:26:23 +0000

    commit Add: 添加 git docker-entrypoint.sh

--- git/docker-entrypoint.sh
+++ git/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Author: wxnacy(wxnacy@gmail.com)
 # Description:
 
@@ -7,4 +7,22 @@ git config --global user.email "${GIT_EMAIL}"
 git config --global credential.helper store
 echo "https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com" > ~/.git-credentials
 
-git $@
+cd /root/work
+
+echo `ls`
+echo $@
+cmd=$1
+if [ $cmd == 'commit' ]
+then
+    arg=$2
+    msg=${@/$1/}
+    msg=${@/$2/}
+    echo  $msg
+    echo 'commit', $arg, $msg
+    git commit $arg "${msg}"
+else
+    git $@
+fi
+
+# git $@
+# /bin/bash
